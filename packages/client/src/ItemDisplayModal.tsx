01a11a044fee52a3305efa726859c5fe1a2600ca
Jupitier Gamer <gamer1lifeinca@gmail.com>
2023-05-22 02:02:01 -0400

    Added masonry to modal

--- packages/client/src/ItemDisplayModal.tsx
+++ packages/client/src/ItemDisplayModal.tsx
@@ -4,6 +4,9 @@ import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from "@mui/material/Button";
+import Masonry from '@mui/lab/Masonry';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
 
 type Props = {
   open: boolean;
@@ -14,13 +17,24 @@ const style = {
   position: 'absolute' as 'absolute',
   top: '50%',
   left: '50%',
+  minHeight: 393,
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 500,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
 };
+
+const heights = [150, 30, 90, 70, 110, 150, 130, 80, 50, 90, 100, 150, 30, 50, 80];
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(0.5),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export function ItemDisplayModal({open, onClose}: Props) {
   return (
@@ -29,13 +43,13 @@ export function ItemDisplayModal({open, onClose}: Props) {
       onClose={onClose}
     >
       <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Text in a modal
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-        </Typography>
-        <Button variant="contained" onClick={onClose}>Close</Button>
+        <Masonry columns={4} spacing={2}>
+          {heights.map((height, index) => (
+            <Item key={index} sx={{ height }}>
+              {index + 1}
+            </Item>
+          ))}
+        </Masonry>
       </Box>
     </Modal>
   )
